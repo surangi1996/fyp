8bef82a5011d2a48b67c0897fe883e5a5ca885e5
surangi1996 <55706550+surangi1996@users.noreply.github.com>
2023-06-05 14:20:27 +0530

    model and end points

--- api/spell_check.py
+++ api/spell_check.py
@@ -2,6 +2,7 @@ from hunspell import Hunspell
 h = Hunspell('en_US')
 
 def suggest(word):
+    print("Word : " + word)
     if not h.spell(word):  # Check spelling with h.check instead of h.spell
         suggestions = h.suggest(word)
         return suggestions
